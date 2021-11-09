cd11acae6b64fd42ca0cdd2ca343dc0e332d4e90
Jeff Titak <jtitak06@yahoo.com>
2021-11-09 16:11:57 +0300

    Exercise complete

--- app.js
+++ app.js
@@ -1,4 +1,41 @@
 console.log("Hello World!\n==========\n");
+function watchTurorialCallback(callback, errorCallback) {
+    let userLeft = false;
+    let userWatchingLiveStream = true;
+  
+    if (userLeft) {
+      errorCallback({
+        name: "User Left",
+        message: ":(",
+      });
+    } else if (userWatchingLiveStream) {
+      callback("Thumbs up and Subscribe");
+    }
+  }
+  
+  watchTurorialCallback(
+    (message) => {
+      console.log(message);
+    },
+    (error) => {
+      console.log(error.name + " " + error.message);
+    }
+  );  
+
 
 // Exercise 1 Section
+let watching = new Promise((resolve, reject) => {
+    let userWatchingLiveStream = false;
+
+    if (userWatchingLiveStream) {
+        resolve("Subscribe.");
+    } else {
+        reject("User left.");
+    }
+});
+
+watching
+.then((result) => console.log(result))
+.catch((error) =>console.log(error));
+
 console.log("EXERCISE 1:\n==========\n");
